e72d23c71434d8eecdaa47e81b6e489059295630
Bruno Alencar <bruno.idealencar@gmail.com>
2018-01-23 13:54:57 -0200

    Add functions to test

--- secret-handshake/secret_handshake.exs
+++ secret-handshake/secret_handshake.exs
@@ -14,18 +14,28 @@ defmodule SecretHandshake do
   10000 = Reverse the order of the operations in the secret handshake
   """
 @binaries [
-    {1,"wink"},
+    {1, "wink"},
     {10, "double blink"},
     {100, "close your eyes"},
-    {1000, "jump"}
+    {1000, "jump"},
   ]
+
+@mapBinaries %{ 1 => "wink", 10 => "double blink", 100 => "close your eyes", 1000 => "jump"}
 
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
+    
+    # Enum.map(@mapBinaries, fn{key, val} -> key == Integer.to_string(code, 2) val end)
+     Integer.to_string(code, 2) |> Map.get(@mapBinaries, & &1) |> Enum.to_list(& &1)
 
-    Enum.list(@binaries, Integer.to_string(code, 2)) |> Enum.into({})
-    # cond do 
-      # code == 1 -> ["wink"] 
+
+    # Map.get(@mapBinaries, 1)
+
+    # @mapBinaries |> Enum.find(fn {key, val} -> key == Integer.to_string(code, 2) end) |> elem(0)
+    # Map.take(@mapBinaries, & &1[Integer.to_string(code, 2)])
+
+    # cond do
+      # code == 1 -> ["wink"]
       # code == 2 -> ["double blink"]
       # code == 4 -> ["close your eyes"]
       # code == 4 -> ["jump"]
