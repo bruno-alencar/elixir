c3f6fad96eec4cb067ea880880fc3149452ba7b9
Bruno Alencar <bruno.idealencar@gmail.com>
2018-02-03 22:08:52 -0200

    Solve rotational cipher exercise

--- rotational-cipher/rotational_cipher.exs
+++ rotational-cipher/rotational_cipher.exs
@@ -9,8 +9,27 @@ defmodule RotationalCipher do
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
       # Enum.map(number, fn{x, v} -> x = text end) 
-      index = Enum.find_index([?a..?z], fn(x) -> x = text end)
-      Enum.find([?a..?z], nil, fn(x) -> x = shift end)
+      # index = [?a..?z]
+      # |> Enum.find_index(fn(x) -> x = "z" end)
+      # |> Enum.at([?a..?z])
+      # |> to_string
+      # [?a..?z]
+      # |> Enum.at(0)
+      # |> to_string
+      # [?a..?z]
+      # |> Enum.at()
+      
+      # Enum.find([?a..?z], nil, fn(x) -> x = shift end)
+     text
+    |> String.to_charlist
+    |> Enum.map(fn(char) ->
+      cond do
+        char in ?a..?z -> ?a + rem(char - ?a + shift, 26)
+        char in ?A..?Z -> ?A + rem(char - ?A + shift, 26)
+        true -> char
+      end
+    end)
+    |> to_string
   end
 end
 
