6cbc983df8bb65c35e2d87f21899b8c4390daa31
Bruno Alencar <bruno.idealencar@gmail.com>
2018-02-10 20:58:03 -0200

    Solve rotational-cipher exercise

--- rotational-cipher/rotational_cipher.exs
+++ rotational-cipher/rotational_cipher.exs
@@ -9,33 +9,16 @@ defmodule RotationalCipher do
 
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
-      array = for n <- ?a..?z, do: << n :: utf8 >>
-
-      index = Enum.find_index(array, fn(x) -> x == text end)
-
-      Enum.at(array, rem(index+shift, 26))
-      # Enum.map(number, fn{x, v} -> x = text end) 
-      # index = [?a..?z]
-      # |> Enum.find_index(fn(x) -> x = "z" end)
-      # |> Enum.at([?a..?z])
-      # |> to_string
-      # [?a..?z]
-      # |> Enum.at(0)
-      # |> to_string
-      # [?a..?z]
-      # |> Enum.at()
-      
-      # Enum.find([?a..?z], nil, fn(x) -> x = shift end)
-    #  text
-    # |> String.to_charlist
-    # |> Enum.map(fn(char) ->
-    #   cond do
-    #     char in ?a..?z -> ?a + rem(char - ?a + shift, 26)
-    #     char in ?A..?Z -> ?A + rem(char - ?A + shift, 26)
-    #     true -> char
-    #   end
-    # end)
-    # |> to_string
+     text
+    |> String.to_charlist
+    |> Enum.map(fn(char) ->
+      cond do
+        char in ?a..?z -> ?a + rem(char - ?a + shift, 26)
+        char in ?A..?Z -> ?A + rem(char - ?A + shift, 26)
+        true -> char
+      end
+    end)
+    |> to_string
   end
 end
 
