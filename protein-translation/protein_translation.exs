d215b701e965c5443036c4dee2556bcdc906c28b
Bruno Alencar <bruno.idealencar@gmail.com>
2018-02-15 22:00:32 -0200

    add item on elixir code on protein translation

--- protein-translation/protein_translation.exs
+++ protein-translation/protein_translation.exs
@@ -2,13 +2,44 @@ defmodule ProteinTranslation do
   @doc """
   Given an RNA string, return a list of proteins specified by codons, in order.
   """
+
+   @mapItems %{  
+  "UGU" => "Cysteine",
+  "UGC" => "Cysteine",
+  "UUA" => "Leucine",
+  "UUG" => "Leucine",
+  "AUG" => "Methionine",
+  "UUU" => "Phenylalanine",
+  "UUC" => "Phenylalanine",
+  "UCU" => "Serine",
+  "UCC" => "Serine",
+  "UCA" => "Serine",
+  "UCG" => "Serine",
+  "UGG" => "Tryptophan",
+  "UAU" => "Tyrosine",
+  "UAC" => "Tyrosine",
+  "UAA" => "STOP",
+  "UAG" => "STOP",
+  "UGA" => "STOP",
+  }
+
   @spec of_rna(String.t()) :: { atom,  list(String.t()) }
-  def of_rna(rna)do
+  def of_rna(rna) do
+
     rna
     |> String.to_charlist
     |> Stream.chunk_every(3)
     |> Enum.to_list
-    |> Enum.map(fn(item) -> to_string(item) |> of_codon end)
+    |> Enum.map(fn(item) ->
+        to_string(item) 
+        |> of_codon
+        |> case do
+              {:ok, res} -> res 
+              err -> err
+           end
+        |> {:ok}
+     end)
+    |> {:ok, all}
   end
 
   @doc """
@@ -33,27 +64,7 @@ defmodule ProteinTranslation do
   UGA -> STOP
   """
 
-  @mapItems %{  
-  "UGU" => "Cysteine",
-  "UGC" => "Cysteine",
-  "UUA" => "Leucine",
-  "UUG" => "Leucine",
-  "AUG" => "Methionine",
-  "UUU" => "Phenylalanine",
-  "UUC" => "Phenylalanine",
-  "UCU" => "Serine",
-  "UCC" => "Serine",
-  "UCA" => "Serine",
-  "UCG" => "Serine",
-  "UGG" => "Tryptophan",
-  "UAU" => "Tyrosine",
-  "UAC" => "Tyrosine",
-  "UAA" => "STOP",
-  "UAG" => "STOP",
-  "UGA" => "STOP",
-  }
-
-  @spec of_codon(String.t()) :: { atom, String.t() }
+@spec of_codon(String.t()) :: { atom, String.t() }
   def of_codon(codon) do
     @mapItems
     |> Map.fetch(codon)
